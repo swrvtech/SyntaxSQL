48b598e9abc0dc95c00712385e07461cf7677f3e
SWRV Tech <admin@swrvtech.com>
2024-06-26 11:50:57 -0400

    E2 examples/reference added

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,7 @@ a
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
 _
 /src/main/java/com/swervtech/hrmsproject/E1.java,8/3/830bdf8c920e73e138eabbdb9714bf1425b74163
+_
+/src/main/java/com/swervtech/hrmsproject/E2.java,7/0/704256ae5f591e2c28b8593ac36cf1434f94e1aa
+_
+/src/main/java/com/swervtech/hrmsproject/E3.java,4/a/4aa91c9f801abfff9f2915d45bceea7ef1e65136
---
+++ src/main/java/com/swervtech/hrmsproject/E3.java
@@ -0,0 +1,43 @@
+package com.swervtech.hrmsproject;
+
+import java.sql.*;
+
+public class E3 {
+    public static void main(String[] args) {
+        // A db url string contains all the reuiqed info to connect to any database
+        String dbURL="jdbc:mysql://3.239.253.255:3306/syntaxhrm_mysql";
+        // username and password to connect to the database
+        String userName="syntax_hrm";
+        String password="syntaxhrm123";
+        try {
+            // creates a connection to the database
+            Connection connection = DriverManager.getConnection(dbURL,userName,password);
+            // creates a statement object to execute the query
+            Statement statement = connection.createStatement();
+            // query to execute
+            String query = "Select * from person";
+            // executes the query and stores the result in a resultset
+            ResultSet resultset = statement.executeQuery(query);
+            // gets the metadata of the resultset
+            ResultSetMetaData rsm = resultset.getMetaData();
+            // gets the number of columns in the resultset
+            for (int i = 1; i < rsm.getColumnCount(); i++) {
+                System.out.print(rsm.getColumnName(i)+" ");
+            }
+            System.out.println();
+            // iterates through the resultset and prints the values
+            while(resultset.next()){
+                for (int i = 1; i < rsm.getColumnCount(); i++) {
+                    System.out.print(resultset.getString(i)+" ");
+                }
+                // prints a new line after each row
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
